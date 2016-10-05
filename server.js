d2332ed4d4c4cc19e380c4691645df009950f9b7
sagarpanchalGithub <sagarpanchal948@gmail.com>
2016-10-05 20:23:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function(req, res){
     res.sendFile('File is served here');
-})
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
