15331416bc4470d83e2c135a14fc18e726bbdc25
sagarpanchalGithub <sagarpanchal948@gmail.com>
2016-11-11 23:48:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.get('/article-one', function(req, res){
     res.send('File is served here');
 });
 
-app.get('/page-two', function(req, res){
-   res.send('This is second page in Web App'); 
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
