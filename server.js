15ebbd8f9a989ec3ad47f702d05ab79e0e1842f4
sagarpanchalGithub <sagarpanchal948@gmail.com>
2016-11-12 22:25:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 app.get('/article-one', function(req, res){
     res.send('File is served here');
 });
-app.get('/Registration-Page', function(req, res){
+app.get('/ui/Registration-Page', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'Registration-Page.html'));
 });
 
